7cb622345e5144402647dbd9ee235a9f2b2d6b0f
Dominic Boggio <dominicboggio@gmail.com>
2019-10-02 23:13:47 -0400

    Added Functionality to Main Menu
    Enter starts from the Main Menu, and Escape exits the game.

---
+++ JeJe1.0/Assets/Scripts/menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit")) // start game (Enter)
+        {
+            SceneManager.LoadScene("Main Scene"); // name of the first scene we want to pull up 
+        }
+        if (Input.GetButtonDown("Cancel")) // exit game (Escape)
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
