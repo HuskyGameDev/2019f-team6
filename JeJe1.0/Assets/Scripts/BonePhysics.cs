bfa1cbfb0064e1a1d6b9d25acf14a7f47e24da9b
Dean Navaroli <deanav777@gmail.com>
2019-10-08 16:43:54 -0400

    PlayerMovement/Bones
    Added PlayerController script.
    Added basic bone physics

---
+++ JeJe1.0/Assets/Scripts/BonePhysics.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonePhysics : MonoBehaviour
+{
+
+    public Rigidbody2D boneBody;
+    public float startForce = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boneBody.freezeRotation = true;
+        boneBody.AddForce(new Vector2(-2*startForce, startForce), ForceMode2D.Force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
