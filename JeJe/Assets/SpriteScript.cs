a2aa511be26756860fca98fedc7f3af86be34000
Dean Navaroli <deanav777@gmail.com>
2019-10-27 21:43:00 -0400

    Update SpriteScript.cs

--- JeJe/Assets/SpriteScript.cs
+++ JeJe/Assets/SpriteScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class SpriteScript : MonoBehaviour
 {
+
+    public SpriteRenderer sprite;
+    public BoxCollider2D player;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if()
     }
 }
