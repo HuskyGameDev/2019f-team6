82099fdfb9bc196d62a244d31cafe4b7226c16c0
Jesse Fischer <54866167+ArtemisLuck@users.noreply.github.com>
2019-10-30 23:56:03 -0400

    Bone daddy idle anim and health bar

---
+++ JeJe/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    public int health = 4;
+    public int numHeads = 4;
+
+    public Image[] heads;
+    public Sprite JeHead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < heads.Length; i++)
+        {
+            if (i < health)
+            {
+                heads[i].sprite = JeHead;
+            }
+
+            if (i < numHeads)
+            {
+                heads[i].enabled = true;
+            }
+            else
+            {
+                heads[i].enabled = false;
+            }
+        }
+    }
+}
