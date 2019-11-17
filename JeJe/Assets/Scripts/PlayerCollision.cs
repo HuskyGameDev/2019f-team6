d508e719b9c249274823a3526cf98f344b0491b0
wesley Robert condie <54695431+wrcondie@users.noreply.github.com>
2019-11-16 16:56:00 -0800

    EndGame
    Working on end condition editted some scripts like healthbar and game manager

--- JeJe/Assets/Scripts/PlayerCollision.cs
+++ JeJe/Assets/Scripts/PlayerCollision.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
-{ 
+{
+    
+
     public AudioClip MusicClip;
 
     //public AudioSource MusicSource;
