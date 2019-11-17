ed2a4a4d8efe8e572c9a7cfbb77d51c72c4fabef
wesley Robert condie <54695431+wrcondie@users.noreply.github.com>
2019-11-16 19:19:10 -0800

    working on winnig

---
+++ JeJe/Assets/Scripts/winCon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winCon : MonoBehaviour
+{
+    public GameManager1 gameManager;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.winGame();
+    }
+}
