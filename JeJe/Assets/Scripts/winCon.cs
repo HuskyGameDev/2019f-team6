6aa7fbc5bc622fa5b33611db733814cab7559868
wesley Robert condie <54695431+wrcondie@users.noreply.github.com>
2019-12-04 18:41:03 -0800

    got win condition working
    checks player x position to switch to win scene.  if player falls through map the player will lose.

--- JeJe/Assets/Scripts/winCon.cs
+++ JeJe/Assets/Scripts/winCon.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class winCon : MonoBehaviour
 {
     public GameManager1 gameManager;
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        gameManager.winGame();
+    public GameObject player;
+    public GameObject daddy;
+    public int tally=0;
+    void Update() {
+
+        if (player.transform.position.x>=18) {
+            gameManager.winGame();
+        }
+        if (player.transform.position.y < -11) {
+            gameManager.endGame();
+        }
     }
+
+  
 }
